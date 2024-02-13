90d896a8b236e2f52b938aec42c11d23f1f176fc
fred <support@qo-op.com>
2024-02-13 18:59:04 +0100

    Try ME /ipfs/QmQZpEUA2pMSqWMg22nevCgb5FqKoDyraJNxqdxTaoNE6V

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 * Juste : PAF/2 + PIF
 * Carré : PAF/4 + PIF²
 
-[ESSAYEZ LA DERNIERE VERSION](http://ipfs.asycn.io/ipfs/QmXzcrtjmA6Ykieu78CuVnkyCiQk2MpQNhV8KMsw67HGKu)
+[ESSAYEZ LA DERNIERE VERSION](http://ipfs.asycn.io/ipfs/QmQZpEUA2pMSqWMg22nevCgb5FqKoDyraJNxqdxTaoNE6V)
--- ipfs.microledger.me.sh
+++ ipfs.microledger.me.sh
@@ -7,6 +7,14 @@
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
+############################################################### ipfs
+##  __  __ ___ ____ ____   ___    _     _____ ____   ____ _____ ____
+## |  \/  |_ _/ ___|  _ \ / _ \  | |   | ____|  _ \ / ___| ____|  _ \
+## | |\/| || | |   | |_) | | | | | |   |  _| | | | | |  _|  _| | |_) |
+## | |  | || | |___|  _ <| |_| | | |___| |___| |_| | |_| | |___|  _ <
+## |_|  |_|___\____|_| \_\\___/  |_____|_____|____/ \____|_____|_| \_\  me
+
+
 MOATS=$(date -u +"%Y%m%d%H%M%S%4N")
 
 OLD=$(cat ${MY_PATH}/.chain)
@@ -23,7 +31,7 @@ echo ${MOATS} > ${MY_PATH}/.moats
 sed -i "s~${OLD}~${TW}~g" ${MY_PATH}/README.md
 sed -i "s~${OLD}~${TW}~g" ${MY_PATH}/index.html
 
-echo '
+echo '# MAKE YOUR GIT IPFS LINKING
 git add .
 git commit -m "Try ME /ipfs/'${TW}'"
 git push
